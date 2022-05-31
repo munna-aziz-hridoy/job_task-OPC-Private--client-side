dfa254461be1b50e6824593c7f2cb4547f97a1f6
munna-aziz-hridoy <munna.aziz.hridoy@gmail.com>
2022-05-31 22:28:10 +0600

    get and show all user in UI

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -2,8 +2,18 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPaperPlane, faPlusSquare } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 import AddDataModal from "./AddDataModal";
+import { useQuery } from "react-query";
+import Spinner from "./Spinner";
 
 const Table = () => {
+  const { data: users, isLoading } = useQuery("users", () => {
+    return fetch("http://localhost:5000/allUser").then((res) => res.json());
+  });
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
   return (
     <>
       <div className="flex justify-end items-center my-5">
@@ -29,36 +39,41 @@ const Table = () => {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th>
-                <label>
-                  <input type="checkbox" className="checkbox" />
-                </label>
-              </th>
-              <td>1</td>
-              <td>
-                <p className="font-bold">Hart Hagerty</p>
-              </td>
-              <td>
-                <p>012345678</p>
-              </td>
-              <td>
-                <p>example@email.com</p>
-              </td>
-              <td>
-                <p>Traveling</p>
-              </td>
-              <th>
-                <div className="flex justify-center items-center gap-2">
-                  <button className="btn btn-xs bg-green-700 border-green-700 text-slate-100">
-                    Update
-                  </button>
-                  <button className="btn btn-xs bg-red-700 border-red-700 text-slate-100">
-                    delete
-                  </button>
-                </div>
-              </th>
-            </tr>
+            {users?.map((user, index) => {
+              const { _id, name, email, hobbies, phone } = user;
+              return (
+                <tr key={_id}>
+                  <th>
+                    <label>
+                      <input type="checkbox" className="checkbox" />
+                    </label>
+                  </th>
+                  <td>{index + 1}</td>
+                  <td>
+                    <p className="font-bold">{name}</p>
+                  </td>
+                  <td>
+                    <p>{phone}</p>
+                  </td>
+                  <td>
+                    <p>{email}</p>
+                  </td>
+                  <td>
+                    <p>{hobbies}</p>
+                  </td>
+                  <th>
+                    <div className="flex justify-center items-center gap-2">
+                      <button className="btn btn-xs bg-green-700 border-green-700 text-slate-100">
+                        Update
+                      </button>
+                      <button className="btn btn-xs bg-red-700 border-red-700 text-slate-100">
+                        delete
+                      </button>
+                    </div>
+                  </th>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>
